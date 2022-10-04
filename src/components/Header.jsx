7ee42f6760ae4b6b5c70e299a42622b976f8845f
Beth-Hollinger <EHollingerFullStackEngineer@gmail.com>
2022-10-04 09:04:42 -0400

    add link to options to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+mport React from 'react'
 import { Link } from "react-router-dom"
 
 export default function Header(props) {
   return (
     <nav className="nav">
-      <Link to="/">
-        <div>VibeVoyage</div>
-      </Link>
+      
+      <ul>
+        <Link to="/notes">
+            <li>Notes</li>
+        </Link>
+        <Link to = "">
+          <li>Login</li>
+        </Link>    
+          <Link to = "">
+          <li>Register</li>
+        </Link>
+      </ul>
+      
     </nav>
   )
 }
+
+
